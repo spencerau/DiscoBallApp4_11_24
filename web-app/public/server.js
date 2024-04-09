06a5916d96b669573b714fd1ac16d71975b26787
Daniel Dinh <dadinh@chapman.edu>
2024-04-09 11:40:16 -0700

    Added in cors to try and do requests

--- web-app/public/server.js
+++ web-app/public/server.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const { Pool } = require('pg');
+const cors = require('cors'); // Import the cors middleware
 
 const app = express();
 const port = 3000;
+
+// Enable CORS for all routes
+app.use(cors());
 
 // Create a PostgreSQL connection pool
 const pool = new Pool({
