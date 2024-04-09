7cb22511a5194f7228cf9fb4c1a71626331389c9
Daniel Dinh <dadinh@chapman.edu>
2024-04-09 12:39:34 -0700

    Updated so the actual endpoints are the same

--- web-app/public/script.js
+++ web-app/public/script.js
@@ -324,7 +324,7 @@ function submitForm() {
     });
     
     // Make an HTTP POST request to the server
-    fetch('https://discoball.vercel.app/api/server',{
+    fetch('https://discoball.vercel.app/submit-form',{
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
